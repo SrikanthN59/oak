45efdefe414cbe261c672fcd1bc1483750c54667
SrikanthN59 <nskanth.12@gmail.com>
2020-04-19 12:41:58 +0200

    First Commit

---
+++ src/main/java/ksh/oak/App.java
@@ -0,0 +1,13 @@
+package ksh.oak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
