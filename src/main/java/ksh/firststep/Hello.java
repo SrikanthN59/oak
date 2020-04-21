4ba737fc751586b402a8340e746fe43ae6844e88
SrikanthN59 <nskanth.12@gmail.com>
2020-04-21 18:28:44 +0200

    JAVA-01 Variables

--- src/main/java/ksh/firststep/Hello.java
+++ src/main/java/ksh/firststep/Hello.java
@@ -4,5 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		int myFristNumber = (10 + 5) + (2 * 10);
+		
+		System.out.println(myFristNumber);
 	}
 }
