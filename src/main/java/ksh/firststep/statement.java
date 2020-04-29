c5b15e56781f6e3223862e563c7c70c2133b90ee
SrikanthN59 <nskanth.12@gmail.com>
2020-04-29 11:49:51 +0200

    Statemets,white Space, indentation..

---
+++ src/main/java/ksh/firststep/statement.java
@@ -0,0 +1,15 @@
+package ksh.firststep;
+
+public class statement {
+	public static void main(String[] args){
+	
+		
+		System.out.println("This is a test");
+
+	System.out.println("This is" +
+		"another"+
+		"still more.");
+	System.out.println("Thsi is another one");
+	
+}
+}
