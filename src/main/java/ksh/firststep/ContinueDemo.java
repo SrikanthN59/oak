c8add6e52de12454ba46d992e1fefa69806f8432
SrikanthN59 <nskanth.12@gmail.com>
2020-05-18 01:08:42 +0200

    ContinueDemo_

---
+++ src/main/java/ksh/firststep/ContinueDemo.java
@@ -0,0 +1,12 @@
+package ksh.firststep;
+
+class ContinueDemo{
+	public static void main(String args[])	{
+		for (int i=1; i<=20; i++) {
+			if(i==5 | i==10 | i==15)
+				continue;
+			System.out.println(i);
+		}
+		
+	}
+}
