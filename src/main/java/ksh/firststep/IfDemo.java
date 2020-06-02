84a95b6bb635111eb5a6d33f1a71fbc35eacc1fe
SrikanthN59 <nskanth.12@gmail.com>
2020-06-02 11:36:22 +0200

    ifdemo..

---
+++ src/main/java/ksh/firststep/IfDemo.java
@@ -0,0 +1,15 @@
+package ksh.firststep;
+
+
+public class IfDemo   {
+	public static void main(String[] args){
+		int value = 2;
+		if (value == 1) {
+			System.out.println("Value was 1");
+		} else if(value == 2){
+			System.out.println("Value was 2");
+		} else {
+			System.out.println("was not 1 or 2");
+	}
+	}
+}
